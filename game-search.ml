75d7fc80c97e2b4f29eb6c82417fee7a597f98f5
Jacob Rosales Chase <coby@case.edu>
2019-10-12 20:21:17 -0400

    Some structural changes

--- game-search.ml
+++ game-search.ml
@@ -15,65 +15,62 @@ open Core
 *)
 
 module Tuple_tools = struct
-  let map_tuple f = function (a, b, c) ->  (f a, f b, f c)
-  let iter_tuple f = function (a, b, c) -> f a; f b; f c
+  let map_tuple f (a, b, c) = (f a, f b, f c)
+  let iter_tuple f (a, b, c) = f a; f b; f c
+  let fold_tuple f (a, b, c) = (f (f a b) c)
+  let hash (a, b, c) = a + 13 * (b * 13 + c)
 end
 
 module type Game =
 sig
-  type state
+  include Comparator.S
   type move
-
-  val solved_state : state
+  
+  val solved_state : t
   val moves : move list
-  val render : state -> unit
+  val render : t -> unit
   val render_move : move -> unit
-  val apply : state -> move -> state
-  val legal : state -> move -> bool
+  val apply : t -> move -> t
+  val legal : t -> move -> bool
 end
-
 
 module Game_Tools (G : Game) :
   (sig
-    val randomize : int -> G.state
-    val execute : G.move list -> G.state -> G.state
+    val randomize_stat : int -> G.t
+    val execute : G.move list -> G.t -> G.t
     val pick_random_moves : int -> ?legal:bool -> G.move list
-    val legal_moves : G.state -> G.move list
+    val legal_moves : G.t -> G.move list
     val render_moves : G.move list -> unit
   end) =
 struct
-  type state = G.state
-
+  include G
+  
   let render_moves moves =
     Printf.printf "[";
-    List.iter ~f:G.render_move moves;
+    List.iter ~f:render_move moves;
     Printf.printf "]\n"
 
-  let legal_moves state = List.filter G.moves ~f:(G.legal state)
+  let legal_moves state = List.filter moves ~f:(legal state)
                  
-  let execute fs init = List.fold ~f:G.apply ~init:init fs
+  let execute fs init = List.fold ~f:apply ~init:init fs
 
   let rec pick_legal_moves_and_execute acc n state legal =
     if n = 0 then (List.rev acc, state)
     else
       let move_set =
         if legal then legal_moves state
-        else G.moves
+        else moves
       in
       let random = List.random_element_exn move_set in
-      let new_state = G.apply state random in
-      G.render state;
-      G.render new_state;
-      G.render_move random;
-      render_moves move_set;
+      let new_state = apply state random in
       pick_legal_moves_and_execute (random::acc) (n - 1) new_state legal
 
-  let randomize n =
-    pick_legal_moves_and_execute [] n G.solved_state true
+  let randomize_stat n =
+    pick_legal_moves_and_execute [] n solved_state true
     |> function (_, x) -> x
 
   let pick_random_moves n ?legal:(legal = true) =
-    pick_legal_moves_and_execute [] n G.solved_state legal
+    pick_legal_moves_and_execute [] n solved_state legal
     |> function (moves,  _) -> moves
 end
 
@@ -81,12 +78,21 @@ end
 
 module Eight_Puzzle : Game =
 struct
-  open Tuple_tools
-  type state = (int * int * int) *
-               (int * int * int) *
-               (int * int * int)
+  include Tuple_tools
+  module T = struct
+    type t = (int * int * int) *
+             (int * int * int) *
+             (int * int * int)  [@@deriving sexp]
+    let f a b =  a + 1693 * b
+    let hash_state x = fold_tuple f (map_tuple hash x)
 
-  type transition = state -> state
+    let compare a b = (hash_state a) - (hash_state b)
+  end
+
+  include T
+  include Comparator.Make(T)
+
+  type transition = t -> t
 
   type move = Left of transition | Right of transition | Up of transition | Down of transition
 
@@ -153,5 +159,22 @@ struct
 end
 
 module Search (G : Game) = struct
-  type t = G.state
+  module Tools = Game_Tools(G)
+  
+  let visited = Set.empty (module G)
+
+  let member = Set.mem visited G.solved_state
+
+  type t = Solved of G.move list
+         | DeadEnd of (G.t, G.comparator_witness) Set.t
+
+  let dfs state visited path =
+    match G.solved_state = state with
+    | true -> Visited path
+    | false ->
+      let f acc move = match acc with
+        | Solved x -> x
+        | Deadend visited -> dfs (G.apply state move) visited (move :: path)
+      in
+      
 end
